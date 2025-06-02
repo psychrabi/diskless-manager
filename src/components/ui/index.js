afb612cf55649d35bf2a9c291aff66a9fc5b6c38
Rabi Sheestha <psychrabi@gmail.com>
2025-06-02 10:21:56 +0545

    added missing exports

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -5,3 +5,7 @@ export { Input } from './Input.jsx';
 export { Select } from './Select.jsx';
 export { Table } from './Table.jsx';
 export { ContextMenu } from './ContextMenu.jsx';
+export { Loading } from './Loading.jsx';
+export { Notification } from './Notification.jsx';
+export { Error } from './Error.jsx';
+
