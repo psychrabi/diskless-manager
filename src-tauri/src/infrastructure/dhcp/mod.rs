7f4beb064d8c7a8bdb135d0fc1d8fc19b77c5a86
Rabi Shrestha <psychrabi@gmail.com>
2026-09-04 18:36:40 +0545

    refactor(pxe): re-export client menu publisher

--- src-tauri/src/infrastructure/dhcp/mod.rs
+++ src-tauri/src/infrastructure/dhcp/mod.rs
@@ -3,4 +3,5 @@ mod implementation;
 mod isc_dhcp;
 
 pub use implementation::*;
+pub(crate) use isc_dhcp::publish_client_ipxe;
 pub use isc_dhcp::{BootReservation, BootReservationPublisher, IscDhcpPublisher};
